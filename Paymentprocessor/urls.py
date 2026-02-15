0c62a90f040dc471a4fbdd66260fec2f693dc062
danielnjama <danielnjama2015@gmail.com>
2026-02-15 08:15:03 +0300

    admin styles

--- Paymentprocessor/urls.py
+++ Paymentprocessor/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("payments.api.urls")),
 ]
+
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns= urlpatterns+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
